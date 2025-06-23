00fefb6f12192157b9f161d897252132744449be
TOMAZ MACHADO DE AQUINO <128157969+Tomazoo1@users.noreply.github.com>
2025-06-23 19:16:58 -0300

    função

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function inserirNome(){
     let nomeUsuario = prompt ("Qual o seu nome?");
-    let elemento = document.querySelector(#nome-usuario);
+    let elemento = document.querySelector("#nome-usuario");
     elemento.textContent = nomeUsuario;
 }
+
+inserirNome();
